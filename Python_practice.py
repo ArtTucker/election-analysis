72b924a0070bf29fde49290718432b0331757148
Art Tucker <art.w.tucker@gmail.com>
2020-11-23 00:40:19 -0800

    Update Python_practice.py

--- Python_practice.py
+++ Python_practice.py
@@ -42,3 +42,11 @@ print(counties)
 
 counties_tuple = ("Arapahoe", "Denver", "Jefferson")
 len(counties_tuple)
+
+counties_dict = {}
+counties_dict["Arapahoe"] = 422829
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
+len(counties_dict)
+
+counties_dict.items()
