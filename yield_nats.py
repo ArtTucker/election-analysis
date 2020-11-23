8d12a585fcdc7ef8967739c161fe50c85d9ae5a2
Art Tucker <art.w.tucker@gmail.com>
2020-11-23 15:22:10 -0800

    Create yield_nats.py

---
+++ yield_nats.py
@@ -0,0 +1,25 @@
+grid = [[0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0]]
+
+import numpy as np
+
+def possible(y,x,n):
+    global grid
+    for i in range(0,9) :
+        if grid[y][x] == 0:
+            for n in range(1,10):
+                if possible(y,x,n):
+                    grid[y][x] = n
+                    solve()
+                    grid[y][x]
+            return
+    print(np.matrix(grid))
+    input("More?")
+
