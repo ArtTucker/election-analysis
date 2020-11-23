e97eaf81dee5460f90e3e624ce5177cd42ea4914
Art Tucker <art.w.tucker@gmail.com>
2020-11-22 23:14:04 -0800

    Update Python_practice.py

--- Python_practice.py
+++ Python_practice.py
@@ -19,3 +19,26 @@ print(len(counties))
 
 counties[0:2]
 
+counties.append("El Paso")
+
+print(counties)
+
+counties.insert(2,"Boulder")
+counties.insert(2,"El Paso")
+
+print(counties)
+
+counties.remove('El Paso')
+print(counties)
+
+counties.pop(-1)
+print(counties)
+
+counties.remove("Boulder")
+
+counties.insert(1,"El Paso")
+counties.remove("Arapahoe")
+print(counties)
+
+counties_tuple = ("Arapahoe", "Denver", "Jefferson")
+len(counties_tuple)
