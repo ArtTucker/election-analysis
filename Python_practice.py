14d1efd78206b13214882b3fc144fc668f509f12
Art Tucker <art.w.tucker@gmail.com>
2020-11-23 19:05:07 -0800

    Update Python_practice.py

--- Python_practice.py
+++ Python_practice.py
@@ -9,15 +9,11 @@ help("keywords")
 
 counties = ["Arapahoe","Denver","Jefferson"]
 
-counties
-
 print(counties)
 
 print(counties[2])
 
 print(len(counties))
-
-counties[0:2]
 
 counties.append("El Paso")
 
@@ -50,3 +46,19 @@ counties_dict["Jefferson"] = 432438
 len(counties_dict)
 
 counties_dict.items()
+counties_dict.keys()
+
+sum(counties_dict.values())
+
+counties_dict.get("Denver")
+counties_dict('Denver')
+
+voting_data = []
+voting_data.append({"county":"Arapahoe","registered_voters":422829})
+voting_data.append({"county":"Denver", "registered_voters":463353})
+voting_data.append({"county":"Jefferson","registered_voters":432438})
+
+len(voting_data)
+
+voting_data.insert(1,{"county":"El Paso","registered_voters":461149})
+voting_data.remove("Arapahoe")
