ce06ac2ae0863e49a050fdac3920e514eead6259
Art Tucker <art.w.tucker@gmail.com>
2020-11-25 01:44:38 -0800

    Update Python_practice.py

--- Python_practice.py
+++ Python_practice.py
@@ -63,3 +63,11 @@ len(voting_data)
 
 voting_data.insert(1,{"county":"El Paso","registered_voters":461149})
 voting_data.remove("Arapahoe")
+
+# How many votes did you get?
+my_votes = int(input("How many votes did you get in the election? "))
+#  Total votes in the election
+total_votes = int(input("What is the total votes in the election? "))
+# Calculate the percentage of votes you received.
+percentage_votes = (my_votes / total_votes) * 100
+print("I received " + str(percentage_votes)+"% of the total votes.")
