305779cb162ad6c879a12603ff86dfb0d06c9344
Art Tucker <art.w.tucker@gmail.com>
2020-11-26 22:52:25 -0800

    Update Python_practice.py

--- Python_practice.py
+++ Python_practice.py
@@ -7,3 +7,17 @@ if 'El Paso' in counties:
     print('El Paso is in the list of counties.')
 else:
     print('El Paso is not in the list of counties.')
+
+if 'Arapahoe' in counties or 'El Paso' in counties:
+    print('Either Arapahoe or El Paso are in the list of counties.')
+else:
+    print('Either Arapahoe or El Paso are not in the list of counties.')
+
+if 'Arapahoe' in counties and 'El Paso' not in counties:
+    print('Only Arapahoe is in the list of counties.')
+else:
+    print('Arapahoe is in the list of counties and El Paso is not in the list.')
+
+for county in counties:
+    print(county)
+
