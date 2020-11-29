2ff05e6de12f00b35432eb8d0056ac91fff3c4cd
Art Tucker <art.w.tucker@gmail.com>
2020-11-28 21:30:04 -0800

    Update PyPoll.py

---
+++ PyPoll.py
@@ -0,0 +1,13 @@
+# The data we need to retrieve
+# 1. The total number of votes cast.
+# 2. A complete list of candidates who received votes.
+# 3. The percentage of votes each candidate won.
+# 4. The total number of votes each candidate won.
+# 5. The winner of the election based on popular vote.
+
+import csv
+dir(csv)
+
+# Assign a variable for the file to load and the path.
+file_to_load = 'resources/election_results.csv'
+
