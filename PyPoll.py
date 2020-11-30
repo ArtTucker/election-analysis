507ab480d86383418dff5b478cec6958ebc0a3a3
Art Tucker <art.w.tucker@gmail.com>
2020-11-29 16:33:44 -0800

    Update PyPoll.py
    Write to outfile

--- PyPoll.py
+++ PyPoll.py
@@ -13,14 +13,20 @@ file_to_load = os.path.join("resources","election_results.csv")
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
-
     # Print the file object
     print(election_data)
 
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Using the open() function with the "w" mode we will write data to the file.
-open(file_to_save,"w")
+
+# Using the with statement open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write some data to the file.
+outfile.write("Hello World! Here is some text.")
+
+# Close the file.
+outfile.close()
+
 
 # To do: perform analysis
 
