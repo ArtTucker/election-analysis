2b59eb9825ae5e5ec7e8b6c698c5e7975e28cdcd
Art Tucker <art.w.tucker@gmail.com>
2020-11-29 23:44:42 -0800

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -16,7 +16,11 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
+    # To do: read and analyze the data here.
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
 
+# To do: perform analysis
 
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
@@ -29,8 +33,6 @@ with open(file_to_save, "w") as txt_file:
 
 # Close the file.
 txt_file.close()
-
-# To do: perform analysis
 
 # Close the file.
 election_data.close()
