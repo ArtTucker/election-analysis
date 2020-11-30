7dd31bdfd7f26ee2c9e94e30fc3de5683afa078b
Art Tucker <art.w.tucker@gmail.com>
2020-11-29 17:35:36 -0800

    Update PyPoll.py
    Reorganized lines

--- PyPoll.py
+++ PyPoll.py
@@ -5,19 +5,18 @@
 # 4. The total number of votes each candidate won.
 # 5. The winner of the election based on popular vote.
 
+# Add dependencies.
 import csv
 import os
 
-# Assign a variable for the file to load and the path.
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("resources","election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
-    # Print the file object
-    print(election_data)
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
