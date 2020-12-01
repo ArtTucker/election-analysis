2e6c93108285ba949f66392258ef8f4202a87be1
Art Tucker <art.w.tucker@gmail.com>
2020-11-30 18:20:26 -0800

    Update PyPoll.py
    Initial variable/list declaration to get candidate names
    If statement to collect unique candidate names.

--- PyPoll.py
+++ PyPoll.py
@@ -13,16 +13,39 @@ import os
 file_to_load = os.path.join("resources","election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Initialize a total vote counter
+total_votes = 0
+# Initialize a list of candidates.
+candidate_options = []
 
 # Open the election results and read the file
 with open(file_to_load) as election_data:
-
-    # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Print the header row.
+    # Read the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+        
+        # Get candidate's name from the row
+        candidate_name = row[2]
+        
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+
+#    for row in file_reader:
+#        print(row)
+
+# Print the candidate list.
+print(candidate_options)
+
+# Print the total votes
+print(total_votes)
 
 
 # To do: perform analysis
